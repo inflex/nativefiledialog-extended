05a8f9bfef9ed19479aec80d4927dd1015582202
Paul L Daniels <pldaniels@gmail.com>
2026-02-16 17:37:16 +1000

    Fixed for SDL3

--- src/include/nfd_sdl2.h
+++ src/include/nfd_sdl2.h
@@ -14,8 +14,19 @@
 #ifndef _NFD_SDL2_H
 #define _NFD_SDL2_H
 
+
+#ifdef _MSC_VER
+#include <SDL.h>
 #include <SDL_error.h>
-#include <SDL_syswm.h>
+#elif defined __APPLE__
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_error.h>
+#else
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_error.h>
+#endif
+
+
 #include <nfd.h>
 #include <stdbool.h>
 
@@ -27,38 +38,39 @@ extern "C" {
 #endif  // __cplusplus
 
 /**
- * Sets the wayland display if the process is running under Wayland, otherwise does nothing.
- * @param sdlWindow The SDL window handle.
- * @return Either true to indicate success, or false to indicate failure.  If false is returned,
- * you can call SDL_GetError() for more information.
- */
-NFD_INLINE bool NFD_SetDisplayPropertiesFromSDLWindow(SDL_Window* sdlWindow) {
-#if defined(SDL_VIDEO_DRIVER_WAYLAND)
-    SDL_SysWMinfo info;
-    SDL_VERSION(&info.version);
-    if (!SDL_GetWindowWMInfo(sdlWindow, &info)) {
-        return false;
-    }
-    if (info.subsystem != SDL_SYSWM_WAYLAND) {
-        return true;
-    }
-    return NFD_SetWaylandDisplay(info.info.wl.display) == NFD_OKAY;
-#else
-    (void)sdlWindow;
-    return true;
-#endif
-}
-
-/**
- * Converts an SDL window handle to a native window handle that can be passed to NFDe.
- * @param sdlWindow The SDL window handle.
- * @param[out] nativeWindow The output native window handle, populated if and only if this function
- * returns true.
- * @return Either true to indicate success, or false to indicate failure.  If false is returned,
+ *  Converts an SDL window handle to a native window handle that can be passed to NFDe.
+ *  @param sdlWindow The SDL window handle.
+ *  @param[out] nativeWindow The output native window handle, populated if and only if this function
+ *  returns true.
+ *  @return Either true to indicate success, or false to indicate failure.  If false is returned,
  * you can call SDL_GetError() for more information.  However, it is intended that users ignore the
  * error and simply pass a value-initialized nfdwindowhandle_t to NFDe if this function fails. */
 NFD_INLINE bool NFD_GetNativeWindowFromSDLWindow(SDL_Window* sdlWindow,
                                                  nfdwindowhandle_t* nativeWindow) {
+
+#if defined(SDL_PLATFORM_WIN32)
+    HWND hwnd = (HWND)SDL_GetPointerProperty(SDL_GetWindowProperties(sdlWindow), SDL_PROP_WINDOW_WIN32_HWND_POINTER, NULL);
+    nativeWindow->type = NFD_WINDOW_HANDLE_TYPE_WINDOWS;
+    nativeWindow->handle = (void*)hwnd;
+        return true;
+#elif defined(SDL_PLATFORM_MACOS)
+    nativeWindow->handle = (void *)SDL_GetPointerProperty(SDL_GetWindowProperties(sdlWindow), SDL_PROP_WINDOW_COCOA_WINDOW_POINTER, NULL);
+    nativeWindow->type = NFD_WINDOW_HANDLE_TYPE_COCOA;
+        return true;
+#elif defined(SDL_PLATFORM_LINUX)
+    if (SDL_strcmp(SDL_GetCurrentVideoDriver(), "x11") == 0) {
+        nativeWindow->handle = (void *)SDL_GetNumberProperty(SDL_GetWindowProperties(sdlWindow), SDL_PROP_WINDOW_X11_WINDOW_NUMBER, 0);
+        nativeWindow->type = NFD_WINDOW_HANDLE_TYPE_X11;
+        return true;
+
+	} else if (SDL_strcmp(SDL_GetCurrentVideoDriver(), "wayland") == 0) {
+        nativeWindow->handle = (void *)SDL_GetNumberProperty(SDL_GetWindowProperties(sdlWindow), SDL_PROP_WINDOW_WIN32_HWND_POINTER, 0);
+        nativeWindow->type = NFD_WINDOW_HANDLE_TYPE_X11;
+    }
+#endif
+
+
+	 /*
     SDL_SysWMinfo info;
     SDL_VERSION(&info.version);
     if (!SDL_GetWindowWMInfo(sdlWindow, &info)) {
@@ -83,18 +95,14 @@ NFD_INLINE bool NFD_GetNativeWindowFromSDLWindow(SDL_Window* sdlWindow,
             nativeWindow->handle = (void*)info.info.x11.window;
             return true;
 #endif
-#if defined(SDL_VIDEO_DRIVER_WAYLAND)
-        case SDL_SYSWM_WAYLAND:
-            nativeWindow->type = NFD_WINDOW_HANDLE_TYPE_WAYLAND;
-            nativeWindow->handle = (void*)info.info.wl.surface;
-            return true;
-#endif
         default:
             // Silence the warning in case we are not using a supported backend.
             (void)nativeWindow;
             SDL_SetError("Unsupported native window type.");
             return false;
     }
+	 */
+	 return false;
 }
 
 #undef NFD_INLINE
